bd7aacfeff8145576994fc94f493112394b6a1fd
LucianoOliveira <Luciano@lucianos-air.lan>
2013-10-29 21:21:57 +0000

    First Commit
    This is how the program was when the tutorial ended

---
+++ Survival/IDKSGameOverScene.h
@@ -0,0 +1,15 @@
+//
+//  IDKSGameOverScene.h
+//  Survival
+//
+//  Created by Luciano Oliveira on 29/10/13.
+//  Copyright (c) 2013 IDKS. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface IDKSGameOverScene : SKScene
+
+-(id)initWithSize:(CGSize)size won:(BOOL)won;
+
+@end
